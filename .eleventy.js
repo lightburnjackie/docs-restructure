8aa649da5cd638ef241ba2f390ea243c7e4aa23e
lightburnjackie <105176012+lightburnjackie@users.noreply.github.com>
2023-02-08 18:47:47 -0500

    modified path prefix for github pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,17 @@
-// const eleventyNavigation = require('@11ty/eleventy-navigation');
 const util = require('util');
-// const quickrefs = require('./src/_data/quickrefs');
-// const markdownIt = require("markdown-it");
+
 const qrShortcode = require('./src/_includes/quickrefShortcode');
 
 module.exports = function (eleventyConfig) {
-
-    // eleventyConfig.setLibrary("md", markdownIt({html: true}));
     
     eleventyConfig.addFilter('dump', obj => {
         return util.inspect(obj)
     });
-
-
     eleventyConfig.addShortcode("quickref", qrShortcode);
-
-    // eleventyConfig.addPlugin(eleventyNavigation);
+    
     return {
         markdownTemplateEngine: "njk",
+        pathPrefix: "/restructure-demo/"
         dir: {
             input: "src",
             output: "docs"
